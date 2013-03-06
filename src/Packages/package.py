a812c36c84ec847717aeee61e8fb120a87f50787
Chris Loew <cloew123@gmail.com>
2013-03-06 10:44:28 -0700

    Added PBF Package to create new Packages

---
+++ src/Packages/package.py
@@ -0,0 +1,22 @@
+import package_manager
+import templates.template_manager as template_manager
+
+import os
+
+class NewPackage:
+    """ Creates a new PBF Package file """
+    category = "new"
+    command = "package"
+    description = "Create a new PBF package file"
+    
+    def run(self, args):
+        """ Run the Package """
+        print "Creating PBF Package:", args[0], "at:", args[1]
+        template_manager.CopyTemplate(args[1], "package.py")
+    
+    def help(self):
+        """ Print the Usage of the New Main Package """
+        print "Usage: pbf new package [name] [path/to/package]"
+        print "\tWill create a PBF Package called [name] at the path given"
+    
+package_manager.RegisterPackage(NewPackage)
