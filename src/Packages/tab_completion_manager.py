99e4ab9bea656a41b77d9d480d0821b6048e4cad
Chris Loew <cloew123@gmail.com>
2013-05-20 21:34:30 -0600

    Added command completion to the tab_completion_manager

--- src/Packages/tab_completion_manager.py
+++ src/Packages/tab_completion_manager.py
@@ -4,6 +4,8 @@ def PrintTabCompletion(packages, arguments):
     results = []
     if len(arguments) == 0:
         results = GetCategoriesForTabCompletion(packages)
+    elif len(arguments) == 1:
+        results = GetCommandsForTabCompletion(packages, arguments[0])
         
     results.sort()
     print " ".join(results)
@@ -11,3 +13,7 @@ def PrintTabCompletion(packages, arguments):
 def GetCategoriesForTabCompletion(packages):
     """ Return a list of strings for tab completion """
     return packages.keys()
+    
+def GetCommandsForTabCompletion(packages, category):
+    """ Return a list of strings for tab completion """
+    return packages[category].keys()
