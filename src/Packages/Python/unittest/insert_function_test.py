c770e6a94a4ff0228f6b941a179944d2626b64ad
Chris Loew <cloew123@gmail.com>
2013-06-23 13:15:07 -0600

    The Insert Function to Test package can now add itself to the suites list

--- src/Packages/Python/unittest/insert_function_test.py
+++ src/Packages/Python/unittest/insert_function_test.py
@@ -5,7 +5,7 @@ import templates.template_manager as template_manager
 class InsertFunctionTest:
     """ Package to Insert a test class for a function into a pre-existing test file """
     category = "insert"
-    command = "func-test"
+    command = "test"
     description = "Insert a Python Function Test Class in a pre-existing Python test file"
     
     def run(self, args):
@@ -24,9 +24,13 @@ class InsertFunctionTest:
         linesRange.reverse()
         for i in linesRange:
             line = originalLines[i]
+            if "suites = [" in line:
+                originalLines = self.addSuiteToSuiteList(originalLines, i, functionToTest)
             if "##########################################################" in line:
                 originalLines[i+1:i+1] = newLines
                 break
+                
+            
         template_manager.Save(testFilename, originalLines)
         
     def getOriginalFileLines(self, testFilename):
@@ -45,10 +49,20 @@ class InsertFunctionTest:
     def writeLines(self, lines, testFilename):
         """ Write the lines to the file """
         template_manager.Save(testFilename, lines)
+        
+    def addSuiteToSuiteList(self, lines, startingLineNumber, functionToTest):
+        """ Add the current suite to the suite List """
+        suiteName = "suite"+Capitalize(functionToTest)
+        
+        for i in range(startingLineNumber, len(lines)):
+            line = lines[i]
+            if  "]" in line:
+                lines[i] = line.replace("]", ",\n          {0}]".format(suiteName))
+                return lines
     
     def help(self):
         """ Print the Usage of the Insert Function Test Package """
-        print "Usage: pbf insert func-test [FunctionToTest] [path/to/test]"
+        print "Usage: pbf insert test [FunctionToTest] [path/to/test]"
         print "\tWill create a new unittest style test class for the given function in the file at the path given"
     
 package_manager.RegisterPackage(InsertFunctionTest)
