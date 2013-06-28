13e6f499f17eb2982544d554a05991f0937d96cd
Chris Loew <cloew123@gmail.com>
2013-06-28 02:10:26 -0600

    Insert Function Test now use the formatted help statement to be DRYer

--- src/Packages/Python/unittest/insert_function_test.py
+++ src/Packages/Python/unittest/insert_function_test.py
@@ -52,8 +52,7 @@ class InsertFunctionTest:
     
     def help(self):
         """ Print the Usage of the Insert Function Test Package """
-        print "Usage: pbf {category} {command}".format(category=self.category, command=self.command)
-        print "Usage: pbf insert test [FunctionToTest] [path/to/test]"
+        print "Usage: pbf {category} {command} [FunctionToTest] [path/to/test]".format(category=self.category, command=self.command)
         print "\tWill create a new unittest style test class for the given function in the file at the path given"
     
 package_manager.RegisterPackage(InsertFunctionTest)
