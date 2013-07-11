5d6dd668f9ac1fc31a21120aeec608ee38c9ef69
Chris Loew <cloew123@gmail.com>
2013-07-11 12:22:25 -0600

    Python will now try and find the proper Package path for an imported file

--- src/helpers/Python/python_helper.py
+++ src/helpers/Python/python_helper.py
@@ -1,4 +1,4 @@
-from helpers.configuration_helper import GetConfigurationsFilename
+from helpers.configuration_helper import GetConfigurationsFilename, GetRelativePathFromConfigurationsDirectory
 from helpers.file_helper import CreateFileIfItDoesNotExist, GetLinesFromFile
 from helpers.filename_helper import GetBaseFilenameWithoutExtension, RemoveFileExtension
 
@@ -6,15 +6,19 @@ import os
 
 def GetPythonPackageForFilename(rootDirectory, filename):
     """ Return the Python Package Path for the given filename from the given Python root """
-    path = os.path.relpath(filename, rootDirectory)
-    path = RemoveFileExtension(path)
-    path = path.replace("/", ".")
-    return path.replace("\\", ".")
+    if rootDirectory is not None:
+        path = os.path.relpath(filename, rootDirectory)
+        path = RemoveFileExtension(path)
+        path = path.replace("/", ".")
+        return path.replace("\\", ".")
+    else:
+        return GetBaseFilenameWithoutExtension(filename)
     
 def GetPythonImportString(filenameToImportFrom, imports):
     """ Constructs and returns a Python Import Line """
-    fromFilename = GetBaseFilenameWithoutExtension(filenameToImportFrom)
-    return "from {0} import {1}".format(fromFilename, ", ".join(imports))
+    packageRoot = GetPythonRootForFilename(filenameToImportFrom)
+    package = GetPythonPackageForFilename(packageRoot, filenameToImportFrom)
+    return "from {0} import {1}".format(package, ", ".join(imports))
 
 def GetPythonRoots():
     """ Return the Python Roots """
@@ -27,3 +31,13 @@ def GetPythonRootsConfigurationsFilename():
     filename = GetConfigurationsFilename("python_roots")
     CreateFileIfItDoesNotExist(filename)
     return filename
+    
+def GetPythonRootForFilename(filename):
+    """ Returns the Python Project Root that the filename is in or None """
+    roots = GetPythonRoots()
+    relativeFilepath = GetRelativePathFromConfigurationsDirectory(filename)
+    for root in roots:
+        if root in relativeFilepath:
+            return os.path.relpath(root)
+    else:
+        return None 
