37c3e70e9119fff3466d296c6bd8c690e78f8dc0
Chris Loew <cloew123@gmail.com>
2013-09-01 15:08:15 -0600

    Updated the new Python main package to accept a directory

--- src/Packages/Python/new_main.py
+++ src/Packages/Python/new_main.py
@@ -1,5 +1,9 @@
+from helpers.file_helper import IsDirectory
 from Packages import package_manager
+
 import templates.template_manager as template_manager
+
+import os
 
 class NewMain:
     """ Creates a new Python Main file """
@@ -11,7 +15,14 @@ class NewMain:
     def run(self, args):
         """ Run the Package """
         print "Creating Python main at:", args[0]
-        template_manager.CopyTemplate(args[0], "Python/main.py")
+        self.makeMain(args[0])
+        
+        
+    def makeMain(self, file):
+        """ Makes the main file at the given location """
+        if IsDirectory(file):
+            file = os.path.join(file, "main.py")
+        template_manager.CopyTemplate(file, "Python/main.py")
     
     def help(self):
         """ Print the Usage of the New Main Package """
