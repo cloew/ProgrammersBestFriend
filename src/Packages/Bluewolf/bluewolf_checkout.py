b48a8d5829b37ba5915881cac66d9fb0cb9d9d9b
Chris Loew <cloew123@gmail.com>
2014-01-18 13:04:25 -0700

    Moved some logic into the Bluewolf svn helper

--- src/Packages/Bluewolf/bluewolf_checkout.py
+++ src/Packages/Bluewolf/bluewolf_checkout.py
@@ -1,7 +1,6 @@
 from Packages import package_manager
 
-from helpers.os_helper import RunCommand
-from subprocess import Popen, PIPE
+from helpers.Bluewolf.svn_helper import GetClients, FindClient, GetClientOrganizationTypes, FindOrganizationType, FindMatchingClients
 
 class BluewolfCheckout:
     """ Checkout a project from Blueowlf """
@@ -22,40 +21,25 @@ class BluewolfCheckout:
         
     def checkout(self, requestedClient, requestedType):
         """ Checkout the SVN folder for the given client and type if possible """
-        
-        clients = self.getClients()
-        client = self.findClient(requestedClient, clients)
+        clients = GetClients()
+        client = FindClient(requestedClient, clients)
         
         if client is not None:
-            clientOrganizationTypes = self.getClientOrganizationTypes(client)
-            type = self.findOrganizationType(requestedType, clientOrganizationTypes)
+            clientOrganizationTypes = GetClientOrganizationTypes(client)
+            type = FindOrganizationType(requestedType, clientOrganizationTypes)
         
-            if type is None:
+            if type is not None:
+                pass
+            else:
                 self.displayOrganizationTypeNotFound(requestedType, clientOrganizationTypes)
         else:
             self.displayClientNotFound(requestedClient, clients)
-            
-    def getClients(self):
-        """ Return the possible clients """
-        return self.getOutputList(["svn", "ls", BluewolfCheckout.svnClientPath])
-        
-    def findClient(self, requestedClient, clients):
-        """ Return the requested Client """
-        return self.findItem(requestedClient, clients)
-        
-    def getClientOrganizationTypes(self, client):
-        """ Return the Client Organization Types """
-        return self.getOutputList(["svn", "ls", BluewolfCheckout.svnClientPath+"/"+client])
-        
-    def findOrganizationType(self, requestedType, organizationTypes):
-        """ Return the Organization Type Directory that matches the requested type """
-        return self.findItem(requestedType, organizationTypes)
         
     def displayClientNotFound(self, requestedClient, clients):
         """ Disaply the possible Clients """
         print "Could not find client:", requestedClient
         print "Did you mean:"
-        for client in self.findMatchingClients(requestedClient, clients):
+        for client in FindMatchingClients(requestedClient, clients):
             print "\t{0}".format(client)
             
     def displayOrganizationTypeNotFound(self, requestedType, organizationTypes):
@@ -64,32 +48,7 @@ class BluewolfCheckout:
         print "Possible Organization Types are:"
         for type in organizationTypes:
             print "\t{0}".format(type)
-            
-    def findMatchingClients(self, requestedClient, clients):
-        """ Return all matching client names """
-        matchingClients = []
-        for client in clients:
-            if requestedClient.lower() in client.lower():
-                matchingClients.append(client)
                 
-        return matchingClients
-            
-    def getOutputList(self, commandList):
-        """ Return the output as a list split on newlines """
-        output = RunCommand(commandList)
-        outputList = output.split('\n')
-        outputList.remove('')
-        return outputList
-        
-    def findItem(self, requestedItem, items):
-        """ Find and return the requested item from the items list """
-        for item in items:
-            cleanedItem = item.replace('/', '')
-            if cleanedItem.lower() == requestedItem.lower():
-                return item
-        else:
-            return None
-    
     def help(self):
         """ Print Package usage """
         print "Usage: pbf {category} {command} [client] [org type] [org name]".format(category=self.category, command=self.command)
---
+++ src/helpers/Bluewolf/svn_helper.py
@@ -0,0 +1,46 @@
+from helpers.os_helper import RunCommand
+
+svnPath = "https://pl5.projectlocker.com/bluewolf/codeblue/svn"
+svnClientPath = svnPath+"/clients"
+
+def GetClients():
+    """ Return the possible clients """
+    return GetOutputList(["svn", "ls", svnClientPath])
+    
+def FindClient(requestedClient, clients):
+    """ Return the requested Client """
+    return FindItem(requestedClient, clients)
+    
+def GetClientOrganizationTypes(client):
+    """ Return the Client Organization Types """
+    return GetOutputList(["svn", "ls", svnClientPath+"/"+client])
+    
+def FindOrganizationType(requestedType, organizationTypes):
+    """ Return the Organization Type Directory that matches the requested type """
+    return FindItem(requestedType, organizationTypes)
+    
+
+def FindMatchingClients(requestedClient, clients):
+    """ Return all matching client names """
+    matchingClients = []
+    for client in clients:
+        if requestedClient.lower() in client.lower():
+            matchingClients.append(client)
+            
+    return matchingClients
+    
+def GetOutputList(commandList):
+    """ Return the output as a list split on newlines """
+    output = RunCommand(commandList)
+    outputList = output.split('\n')
+    outputList.remove('')
+    return outputList
+    
+def FindItem(requestedItem, items):
+    """ Find and return the requested item from the items list """
+    for item in items:
+        cleanedItem = item.replace('/', '')
+        if cleanedItem.lower() == requestedItem.lower():
+            return item
+    else:
+        return None
