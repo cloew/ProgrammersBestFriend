e5acc77f9504a9770c8ba31991d17d5822e76176
Chris Loew <cloew123@gmail.com>
2014-01-25 12:43:23 -0700

    Removed KaoGUI from core install

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(name='pbf',
       packages=['pbf',
                 'pbf.helpers', 'pbf.helpers.Bluewolf', 'pbf.helpers.Project', 'pbf.helpers.Python',
                     'pbf.helpers.Python.unittest', 'pbf.helpers.Salesforce', 'pbf.helpers.XML',
-                'pbf.Commands', 'pbf.Commands.Bluewolf', 'pbf.Commands.Core', 'pbf.Commands.KaoGUI',
-                'pbf.Commands.PBF', 'pbf.Commands.Project', 'pbf.Commands.Python', 'pbf.Commands.Python.PySide', 'pbf.Commands.Python.unittest',  
+                'pbf.Commands', 'pbf.Commands.Bluewolf', 'pbf.Commands.Core', 'pbf.Commands.PBF', 
+                'pbf.Commands.Project', 'pbf.Commands.Python', 'pbf.Commands.Python.PySide', 'pbf.Commands.Python.unittest',  
                 'pbf.Commands.Salesforce',
                 'pbf.templates'],
-      package_data = {'pbf.templates':['KaoGUI/*', 'PBF/*', 'Python/PySide/*', 'Python/unittest/*', 'Python/*.py', 'Salesforce/*']},
+      package_data = {'pbf.templates':['PBF/*', 'Python/PySide/*', 'Python/unittest/*', 'Python/*.py', 'Salesforce/*']},
       scripts=['pbf/scripts/pbf']
      )
