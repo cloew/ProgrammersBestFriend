5c4a8ae2ba264e2ee5de887d6899f929dfa84144
Chris Loew <cloew123@gmail.com>
2014-01-27 19:44:07 -0700

    Removed SAlesforce specific functionality

--- pbf/helpers/Salesforce/salesforce_project_helper.py
+++
@@ -1,7 +0,0 @@
-from pbf.helpers.Project.project_helper import GetParentProjectFromDirectory
-from pbf.helpers.Salesforce.salesforce_project import SalesforceProject
-
-def GetCurrentSalesforceProject():
-    """ Get Salesforce Project for the current directory """
-    project = GetParentProjectFromDirectory()
-    return SalesforceProject(project)
