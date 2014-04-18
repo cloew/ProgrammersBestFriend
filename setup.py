b2a489eb9d092d57c6c404fb57c9c8f081fb0d9a
Chris Loew <cloew123@gmail.com>
2014-04-18 17:51:38 -0600

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(name='pbf',
-      version='.1',
+      version='.2',
       description="Programmer's Best Friend Command Line Utility",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
       #url='http://www.python.org/sigs/distutils-sig/',
       packages=['pbf',
                 'pbf.helpers', 'pbf.helpers.Project', 'pbf.helpers.XML',
-                'pbf.Commands', 'pbf.Commands.Core', 
+                'pbf.Commands', 'pbf.Commands.CommandDirectory', 'pbf.Commands.Core', 
                 'pbf.Commands.PBF', 'pbf.Commands.Project', 
                 'pbf.Commands.Python',
                 'pbf.templates'],
