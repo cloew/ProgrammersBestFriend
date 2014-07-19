10091cbd5c012b4281e00fce7eefc188c0c9ad28
Chris Loew <cloew123@gmail.com>
2014-07-19 14:23:49 -0600

    Incremented version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='pbf',
-      version='.2',
+      version='.3',
       description="Programmer's Best Friend Command Line Utility",
       author='Chris Loew',
       author_email='cloew123@gmail.com',
