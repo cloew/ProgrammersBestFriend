e4cc1e823616817ac479249ab9001b23d500054c
Chris Loew <cloew123@gmail.com>
2015-02-16 17:40:15 -0700

    The New Pbf Properties Command now complies with Command Map

--- pbf/Commands/PBF/new_pbf_properties.py
+++ pbf/Commands/PBF/new_pbf_properties.py
@@ -1,14 +1,9 @@
-from pbf.Commands import command_manager
-
 from pbf.helpers.file_helper import Save
 
 import os
 
 class NewPbfProperties:
-    """ COmmand to create new pbf properties file """
-    category = "new"
-    command = "pbf-properties"
-    description = "Creates a new empty PBF Properties file"
+    """ Command to create new pbf properties file """
     
     def addArguments(self, parser):
         """ Add arguments to the parser """
@@ -23,10 +18,3 @@ class NewPbfProperties:
         pathToProperties = os.path.join(path, '.pbf-properties')
         Save(pathToProperties)
         return pathToProperties
-    
-    def help(self):
-        """ Print Command usage """
-        print "Usage: pbf {category} {command} [path]".format(category=self.category, command=self.command)
-        print "Creates a new empty PBF Properties file at the path specified"
-    
-command_manager.RegisterCommand(NewPbfProperties)
--- pbf/command_map.py
+++ pbf/command_map.py
@@ -2,6 +2,7 @@ from pbf.Commands.command_manager import CommandConfig, RegisterCommands
 
 commands = [CommandConfig('open', 'pbf.Commands.open.Open', description="Open the provided file in the default editor."),
             CommandConfig('insert pbf-package', 'pbf.Commands.PBF.insert_pbf_package.InsertPbfPackage', description="Insert a PBF Package into the Properties file"),
-            CommandConfig('install tab-completion', 'pbf.Commands.PBF.install_tab_completion.InstallTabCompletion', description="Install the PBF Tab Completion")]
+            CommandConfig('install tab-completion', 'pbf.Commands.PBF.install_tab_completion.InstallTabCompletion', description="Install the PBF Tab Completion"),
+            CommandConfig('new pbf-properties', 'pbf.Commands.PBF.new_pbf_properties.NewPbfProperties', description="Creates a new empty PBF Properties file")]
 
 RegisterCommands(commands)
