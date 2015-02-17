6a55171042023c87ce80e73f8250ddf5fb71bfce
Chris Loew <cloew123@gmail.com>
2015-02-16 17:46:08 -0700

    Open Project File Command now complies with command map

--- pbf/Commands/Project/open_project_file.py
+++ pbf/Commands/Project/open_project_file.py
@@ -1,12 +1,7 @@
 from pbf.helpers.Project.project_helper import GetParentProjectFromDirectory
 
-from pbf.Commands import command_manager
-
 class OpenProjectFile:
     """ Open a file within the current Project Context """
-    category = "project"
-    command = "open"
-    description = "Open a file within the currrent project"
     
     def addArguments(self, parser):
         """ Add arguments to the parser """
@@ -22,10 +17,3 @@ class OpenProjectFile:
     def open(self, filename, project):
         """ Open the file for the given project """
         project.open(filename)
-    
-    def help(self):
-        """ Print Command usage """
-        print "Usage: pbf {category} {command} [file]".format(category=self.category, command=self.command)
-        print "Opne the given file using the project's defined editor"
-    
-command_manager.RegisterCommand(OpenProjectFile)
--- pbf/command_map.py
+++ pbf/command_map.py
@@ -4,6 +4,7 @@ commands = [CommandConfig('open', 'pbf.Commands.open.Open', description="Open th
             CommandConfig('insert pbf-package', 'pbf.Commands.PBF.insert_pbf_package.InsertPbfPackage', description="Insert a PBF Package into the Properties file"),
             CommandConfig('install tab-completion', 'pbf.Commands.PBF.install_tab_completion.InstallTabCompletion', description="Install the PBF Tab Completion"),
             CommandConfig('new pbf-properties', 'pbf.Commands.PBF.new_pbf_properties.NewPbfProperties', description="Creates a new empty PBF Properties file"),
-            CommandConfig('new project', 'pbf.Commands.Project.new_project.NewProject', description="Create a new PBF Project")]
+            CommandConfig('new project', 'pbf.Commands.Project.new_project.NewProject', description="Create a new PBF Project"),
+            CommandConfig('project open', 'pbf.Commands.Project.open_project_file.OpenProjectFile', description="Open a file within the currrent project")]
 
 RegisterCommands(commands)
